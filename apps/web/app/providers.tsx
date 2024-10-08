2cfcd1b48c1954953fcde48df32a2b67f0ea6dfa
김보경 <rlaqhrud6263@naver.com>
2024-10-08 23:54:59 +0900

    Feat: tanstack query 설정 추가

---
+++ apps/web/app/providers.tsx
@@ -0,0 +1,38 @@
+import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  }
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,11 +1,14 @@
 module.exports = {
   printWidth: 120,
-  trailingComma: "all", // 기본값
-  tabWidth: 2, // 기본값
-  semi: true, // 일부 코드에서 라인의 시작 부분에 세미 콜론 추가
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
   singleQuote: false,
-  bracketSpacing: true, // 기본값. true인 경우 {foo:bar}는 { foo: bar }로 변환됨
-  arrowParens: "always", // 기본값
-  useTabs: false, // 기본값
+  bracketSpacing: true,
+  arrowParens: "always",
+  useTabs: false,
   endOfLine: "auto",
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
 };
