6a96c909fc85c9ef5fc37bacf4b10da87423dc95
cindyCho <hjcho2303@naver.com>
2024-10-13 22:11:31 +0900

    Chore: 수정

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,13 +5,6 @@ import Input from "@ui/src/components/common/Input";
 export default function Home(): JSX.Element {
   return (
     <div>
-      {/* <button type="button" className="bg-primary">
-        tailwind 확인용
-      </button>
-      <button type="button" className="bg-primary">
-        husky 확인용
-      </button>
-      <div className="w-10">px 확인용</div> */}
       <div className="flex flex-col gap-20 m-30">
         <Input id="name" placeholder="참여자" />
         <Input id="name" placeholder="참여자" isError errorMessage="에러 예시" />
