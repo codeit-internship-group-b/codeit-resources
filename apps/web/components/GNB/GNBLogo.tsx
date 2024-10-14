79d15ee23288277ab58ed309bef9d52859052b6b
cindyCho <hjcho2303@naver.com>
2024-10-14 14:15:07 +0900

    Refactor: 로고 classname으로 수정

--- apps/web/components/GNB/GNBLogo.tsx
+++ apps/web/components/GNB/GNBLogo.tsx
@@ -3,8 +3,8 @@ import { LogoCodeit, LogoText } from "@repo/ui/public";
 export default function NavLogo() {
   return (
     <div className="hidden md:flex gap-8 items-center p-8 mb-10">
-      <LogoCodeit width={26} height={26} />
-      <LogoText width={85} height={11} />
+      <LogoCodeit className="w-26 h-26" />
+      <LogoText className="w-85 h-11" />
     </div>
   );
 }
