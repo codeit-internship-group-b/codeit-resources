4be9aee40d55933db2b2910b5bdda96c2582ded0
YoungJun Bae <dudwns0213@gmail.com>
2024-10-15 15:37:53 +0900

    FE-Feat: Button 컴포넌트 cn 함수 적용, style 추가

--- packages/ui/src/components/common/Button/index.tsx
+++ packages/ui/src/components/common/Button/index.tsx
@@ -1,8 +1,10 @@
 import React, { ComponentProps } from "react";
-import clsx from "clsx";
+// 기존의 clsx 임포트를 제거하고 cn 함수를 임포트합니다.
+// import clsx from "clsx";
+import cn from "@ui/src/utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {
-  variant: "Action" | "primary" | "secondary" | "Tertiary" | "Text";
+  variant: "Action" | "primary" | "secondary" | "Tertiary" | "TertiaryColor" | "Text" | "TextColor";
   isActive?: boolean;
   className?: string;
   children: React.ReactNode;
@@ -11,46 +13,44 @@ interface ButtonProps extends ComponentProps<"button"> {
 export default function Button(props: ButtonProps) {
   const { variant, isActive = true, className = "", children, ...rest } = props;
 
-  // 기본 버튼 스타일 (Base button styles)
-  const baseClassName = "";
+  // 공통 스타일
+  const baseStyles = "center flex items-center justify-center transition-all duration-300 ease-linear transform";
 
-  const variantClassName = clsx(
-    // 공통 스타일 (Common styles)
-    "center",
+  // 비활성화 상태일 때 공통 스타일
+  const inactiveStyles = "bg-gray-200/10 text-custom-black/30 cursor-not-allowed hover:bg-gray-200/10";
 
-    // variant에 따른 스타일
-    {
-      // Action 버튼 스타일
-      "text-lg-bold border-custom-black md:text-2lg-bold flex h-40 w-full items-center justify-center rounded-lg border-2 px-20 py-8 text-center md:h-48 md:px-32 md:py-10 md:w-auto":
-        variant === "Action",
-      [isActive
-        ? "bg-purple-400 text-white/90 hover:bg-purple-800 hover:text-white"
-        : "bg-gray-200/10 text-custom-black/30 cursor-not-allowed hover:bg-gray-200/10"]: variant === "Action",
+  // 변형별 기본 스타일
+  const variantStyles: Record<ButtonProps["variant"], string> = {
+    Action:
+      "text-lg-bold md:text-2lg-bold h-40 w-full rounded-lg border-2 px-20 py-8 text-center md:h-48 md:px-32 md:py-10 md:w-auto border-custom-black",
+    primary:
+      "min-w-68 h-32 text-sm-medium md:min-w-84 md:h-40 md:text-md-medium lg:min-w-100 lg:h-42 gap-4 rounded-8 px-12 py-6 md:rounded-lg md:px-16 md:py-8 lg:px-24 lg:py-8",
+    secondary:
+      "min-w-68 h-32 text-sm-medium md:min-w-84 md:h-40 md:text-md-medium lg:min-w-100 lg:h-42 gap-4 rounded-8 border-1 border-custom-black/20 px-12 py-6 md:rounded-lg md:px-16 md:py-8 lg:px-24 lg:py-8",
+    Tertiary:
+      "h-24 px-10 py-4 rounded-full gap-4 md:h-28 md:px-12 md:py-4 md:gap-4 lg:h-32 lg:px-16 lg:py-5 border-1 border-custom-black/20 text-xxs-medium md:text-xs-medium lg:text-md-medium",
+    TertiaryColor:
+      "h-24 px-10 py-4 rounded-full gap-4 md:h-28 md:px-12 md:py-4 md:gap-4 lg:h-32 lg:px-16 lg:py-5 border-1 border-purple-700/40 text-xxs-medium",
+    Text: "h-28 py-4 px-6 gap-2 rounded-6 text-xs-medium md:h-32 md:py-4 md:px-8 md:text-md-medium md:gap-4",
+    TextColor: "h-28 py-4 px-6 gap-2 rounded-6 text-xs-medium md:h-32 md:py-4 md:px-8 md:text-md-medium md:gap-4",
+  };
 
-      // primary 버튼 스타일
-      "min-w-68 text-sm-medium md:min-w-84 md:text-md-medium lg:min-w-100 lg:h-42 flex h-32 items-center justify-center gap-4 rounded-md px-12 py-6 md:h-40 md:rounded-lg md:px-16 md:py-8 lg:rounded-lg lg:px-24 lg:py-8":
-        variant === "primary",
-      [isActive
-        ? "bg-purple-400 text-white hover:bg-purple-800"
-        : "bg-gray-200/10 text-custom-black/30 cursor-not-allowed hover:bg-gray-200/10"]: variant === "primary",
+  // 변형별 활성화 상태일 때 스타일
+  const activeStyles: Record<ButtonProps["variant"], string> = {
+    Action: "bg-purple-400 text-white/90 hover:bg-purple-800 hover:text-white",
+    primary: "bg-purple-400 text-white hover:bg-purple-800",
+    secondary: "bg-white/40 text-custom-black/80 hover:bg-custom-black/5 hover:text-custom-black",
+    Tertiary: "bg-white/40 text-custom-black/80 hover:bg-custom-black/5 hover:text-custom-black",
+    TertiaryColor: "bg-white/40 text-purple-700 hover:bg-purple-700/40 hover:text-purple-900",
+    Text: "bg-white/0 text-custom-black/80 hover:text-custom-black hover:bg-custom-black/5",
+    TextColor: "bg-white/0 text-purple-900 hover:text-purple-600 hover:bg-purple-700/10",
+  };
 
-      // secondary 버튼 스타일
-      "min-w-68 text-sm-medium md:min-w-84 md:text-md-medium lg:min-w-100 lg:h-42 flex h-32 items-center border-1 border-custom-black/20 justify-center gap-4 rounded-md px-12 py-6 md:h-40 md:rounded-lg md:px-16 md:py-8 lg:rounded-lg lg:px-24 lg:py-8":
-        variant === "secondary",
-      [isActive
-        ? "bg-white/40 text-custom-black/80 hover:bg-custom-black/5 hover:text-custom-black"
-        : "bg-gray-200/10 text-custom-black/30 cursor-not-allowed hover:bg-gray-200/10"]: variant === "secondary",
-
-      // Tertiary 버튼 스타일
-      "text-gray-700 bg-transparent": variant === "Tertiary",
-
-      // Text 버튼 스타일
-      "bg-transparent text-blue-500 underline": variant === "Text",
-    },
-  );
+  // 최종 클래스 이름 조합
+  const classes = cn(baseStyles, variantStyles[variant], isActive ? activeStyles[variant] : inactiveStyles, className);
 
   return (
-    <button className={clsx(baseClassName, className, variantClassName)} {...rest}>
+    <button className={classes} {...rest}>
       {children}
     </button>
   );
