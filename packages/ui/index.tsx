cece02cc3324ed26d1148154894600e02ccb4f08
dudwns0213 <dudwns0213@gmail.com>
2024-10-17 04:13:45 +0900

    FE-Feat: Label index export

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,4 +1,6 @@
 export { default as Toast } from "@ui/src/components/common/Toast/index";
 export { default as notify } from "@ui/src/components/common/Toast/notify";
 
+export { default as Label } from "@ui/src/components/common/Label/index";
+
 // export * from "@ui/components/ui/button"
