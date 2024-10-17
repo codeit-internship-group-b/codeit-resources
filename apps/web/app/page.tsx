1f34ad04419a363123a8f8d006861b0359b45ea8
YoungJun Bae <dudwns0213@gmail.com>
2024-10-17 15:00:55 +0900

    FE-Feat: client error로 인한 Modal 사용 페이지 변경

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,10 @@
-import { Modal } from "@ui/index";
-
 export default function Home(): JSX.Element {
   return (
     <>
-      <Modal.Root>
-        <Modal.Trigger>modal trigger</Modal.Trigger>
-        <Modal.Content>
-          <Modal.Title>제목</Modal.Title>
-          <Modal.Description>내용</Modal.Description>
-          <Modal.Close
-            onConfirm={() => {
-              console.log("Button Clicked!");
-            }}
-          >
-            예
-          </Modal.Close>
-        </Modal.Content>
-      </Modal.Root>
+      <div />
+      <div />
+      <div />
+      <div />
       <div />
     </>
   );
