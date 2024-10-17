2be2b1fea3070c4bfbae0a4a7f695f27ff7e7627
YoungJun Bae <dudwns0213@gmail.com>
2024-10-17 17:30:44 +0900

    FE-Feat: Trigger useEffect 배열 최적화

--- packages/ui/src/components/common/Modal/Trigger.tsx
+++ packages/ui/src/components/common/Modal/Trigger.tsx
@@ -27,7 +27,7 @@ export default function ModalTrigger(props: ModalTriggerProps): JSX.Element {
     if (triggerRef.current) {
       setTrigger(triggerRef);
     }
-  }, [triggerRef, setTrigger]);
+  }, [setTrigger]);
 
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
