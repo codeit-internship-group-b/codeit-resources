d9a923e33c4bea59bed4d04a7e22d67010c65f05
YoungJun Bae <dudwns0213@gmail.com>
2024-10-18 14:19:08 +0900

    FE-Feat: Sidebar 컴포넌트 구현 및 css

---
+++ apps/web/components/common/Sidebar/Layout.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useState } from "react";
+import Sidebar from "./index";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+function Layout(props: LayoutProps): JSX.Element {
+  const { children } = props;
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          setIsSidebarOpen(true);
+        }}
+        className="bg-blue-500 p-2 text-white"
+      >
+        사이드바 열기
+      </button>
+
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onClose={() => {
+          setIsSidebarOpen(false);
+        }}
+      />
+
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ apps/web/components/common/Sidebar/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import { DoubleChevron } from "@ui/public";
+
+interface SidebarProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function Sidebar(props: SidebarProps): JSX.Element {
+  const { isOpen, onClose } = props;
+  return (
+    <div
+      className={`min-w-400 border-l-1 border-custom-black/20 fixed right-0 top-0 h-full transform bg-white shadow-lg transition-transform duration-300 ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <button
+        type="button"
+        className="my-8 ml-8 size-40 cursor-pointer p-8"
+        onClick={() => {
+          onClose();
+        }}
+      >
+        <DoubleChevron />
+      </button>
+
+      <div className="px-32">사이드바 내용입니당</div>
+    </div>
+  );
+}
+
+export default Sidebar;
