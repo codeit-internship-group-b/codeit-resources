4f953f6817fa7d71acb6c2dbba38a0102c7d115a
YoungJun Bae <dudwns0213@gmail.com>
2024-10-18 14:30:05 +0900

    FE-Remove: providers test code delete

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,7 +3,6 @@
 import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { type ReactNode } from "react";
-import { Modal } from "@ui/index";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -35,25 +34,6 @@ export function Providers({ children }: ProvidersProps): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <Modal.Root>
-        <Modal.Trigger>modal trigger</Modal.Trigger>
-        <Modal.Content>
-          <Modal.Title>제목</Modal.Title>
-          <Modal.Description>
-            기존의 자리는 취소되며,
-            <br /> 선택한 자리가 예약됩니다.
-          </Modal.Description>
-          <Modal.Close
-            onConfirm={() => {
-              console.log("Button Clicked!");
-            }}
-            confirmText="이동하기"
-            cancelText="취소하기"
-          >
-            예
-          </Modal.Close>
-        </Modal.Content>
-      </Modal.Root>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
