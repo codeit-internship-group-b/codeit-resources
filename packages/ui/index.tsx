2812c2ca5151cde090f3c384dae9addbe441cbfe
YoungJun Bae <dudwns0213@gmail.com>
2024-10-21 15:28:41 +0900

    [Feat] 공통컴포넌트 RadioButton 제작 (#52)
    * FE-Feat: Button storybook 수정
    
    * FE-Chore: 기초 파일 및 storybook 생성
    
    * FE-Feat: RadioButton interface 지정
    
    * FE-Feat: RadioGroup, Option 컴포넌트 구현
    
    * FE-Feat: Radio component ui index에서 export
    
    * FE-Test: web page test
    
    * FE-Remove: clsx 중복으로 인한 삭제(2개 존재)
    
    * FE-Setting: ui jest.config.cjs ignorePatterns 추가
    
    * FE-Feat: output export로 인한 providers에서 test
    
    * FE-Remove: providers test 삭제
    
    * FE-Feat: type정리 및 eslint 수정
    
    * FE-Feat: null type 검사 암묵적 변환 유도
    
    * FE-Feat: RadioGroup fieldset과 legend 태그 사용

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,5 +1,6 @@
 export { default as Toast } from "@ui/src/components/common/Toast/index";
 export { default as notify } from "@ui/src/components/common/Toast/notify";
+export { default as Radio } from "@ui/src/components/common/RadioButton/index";
 export { default as Modal } from "@ui/src/components/common/Modal/index";
 
 export { default as Badge } from "@ui/src/components/common/Badge/index";
