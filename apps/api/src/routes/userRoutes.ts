350f0f6c720a08b1b5461043dca011465312601c
김보경 <rlaqhrud6263@naver.com>
2024-10-21 21:32:21 +0900

    BE-Feat: errorHandler 추가

--- apps/api/src/routes/userRoutes.ts
+++ apps/api/src/routes/userRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-// import * as userController from "../controllers/userController";
 // import { adminCheckMiddleware } from "../middleware/adminCheckMiddleware";
 
 const userRouter: Router = Router();
-// const { getUsers, getUser, createUser, updateUser, deleteUser } = userController;
 
 // userRouter.get("/", getUsers);
-// userRouter.get("/:id", getUser);
+// userRouter.get("/:userId", getUser);
 // userRouter.post("/create", adminCheckMiddleware, createUser);
 // userRouter.put("/:id", adminCheckMiddleware, updateUser);
 // userRouter.delete("/:id", adminCheckMiddleware, deleteUser);
