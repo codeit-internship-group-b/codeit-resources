997a24a1ef103413cd05a74532e759e3e7f4fadd
YoungJun Bae <dudwns0213@gmail.com>
2024-10-21 16:34:15 +0900

    FE-Setting: react modal sheet install

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
 import SideLayout from "@/components/common/Sidebar/Layout";
+import BottomSheet from "@/components/common/BottomSheet";
 
 export default function Home(): JSX.Element {
   return (
-    <SideLayout>
-      <h1>홈 페이지</h1>
-    </SideLayout>
+    <>
+      <SideLayout>
+        <h1>홈 페이지</h1>
+      </SideLayout>
+      <BottomSheet />
+    </>
   );
 }
