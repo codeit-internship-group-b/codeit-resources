c536af7c50f0ae888b90b0ce12e807a997eaff3a
cindyCho <hjcho2303@naver.com>
2024-10-22 17:16:03 +0900

    FE-Style: design 수정

--- apps/web/app/dashboard/_components/DashboardSection.tsx
+++ apps/web/app/dashboard/_components/DashboardSection.tsx
@@ -14,9 +14,11 @@ interface ItemInfo {
 }
 
 export default function DashboardSection({ type, data }: DashboardSectionProps): JSX.Element {
+  const isMeeting = type === "meeting";
+
   return (
     <div>
-      <h1 className="mb-16">{type === "meeting" ? "내 회의" : "내 장비"}</h1>
+      <h1 className="mb-16">{isMeeting ? "내 회의" : "내 장비"}</h1>
       <hr className="mb-16 border-gray-200/10" />
       {data.length > 0 ? (
         <div className="flex gap-16 overflow-auto">
@@ -25,7 +27,7 @@ export default function DashboardSection({ type, data }: DashboardSectionProps):
               key={item.title}
               className={cn(
                 "rounded-8 text-custom-black/80 mb-4 border border-solid border-gray-200/10 p-8",
-                type === "meeting" ? "min-w-259 w-259 h-148" : "min-w-240 w-240 h-144",
+                isMeeting ? "min-w-259 w-259 h-148 md:w-275 md:h-172" : "min-w-240 w-240 h-144 md:w-280 md:h-160",
               )}
             >
               <div className="relative bottom-6 text-right">
@@ -36,37 +38,35 @@ export default function DashboardSection({ type, data }: DashboardSectionProps):
               <div
                 className={cn(
                   "grid grid-rows-2",
-                  type === "meeting" ? "relative bottom-6 gap-4 pl-24" : "relative bottom-6 justify-center gap-8",
+                  isMeeting
+                    ? "relative bottom-6 gap-4 pl-24 md:bottom-0 md:gap-8"
+                    : "relative bottom-6 justify-center gap-8 md:bottom-2 md:gap-12",
                 )}
               >
-                {type === "meeting" && <div className="text-2lg-bold">{item.title}</div>}
-                <time className={cn("text-13 leading-21", type === "meeting" ? "" : "order-1")}>{item.time}</time>
-                <div className={cn(type === "meeting" ? "" : "text-center")}>
-                  <span
-                    className={cn(
-                      "rounded-32 border-custom-black/5 !text-sm-bold border border-solid bg-purple-100 px-8 py-4 text-purple-300",
-                    )}
-                  >
+                {isMeeting ? <div className="text-2lg-bold">{item.title}</div> : null}
+                <time className={cn("text-13 leading-21", !isMeeting && "order-1")}>{item.time}</time>
+                <div className={cn(!isMeeting && "text-center")}>
+                  <span className="rounded-32 border-custom-black/5 !text-sm-bold border border-solid bg-purple-100 px-8 py-4 text-purple-300">
                     {item.resource}
                   </span>
                 </div>
               </div>
-              <div className={cn(type === "meeting" ? "pr-8 text-right" : "pt-8 text-center")}>
+              <div className={cn(isMeeting ? "pr-8 text-right" : "pt-8 text-center")}>
                 <button
                   type="button"
                   className={cn(
                     "rounded-6 text-sm-medium border-custom-black/20 relative border border-solid px-12 py-5",
-                    type === "meeting" ? "-top-20" : "-top-10",
+                    isMeeting ? "-top-20 md:-top-4" : "-top-10 md:-top-2",
                   )}
                 >
-                  {type === "meeting" ? "회의 종료" : "장비 반납"}
+                  {isMeeting ? "회의 종료" : "장비 반납"}
                 </button>
               </div>
             </div>
           ))}
         </div>
       ) : (
-        <EmptyState type={type === "meeting" ? "meeting" : "equipment"} />
+        <EmptyState isMeeting={isMeeting} />
       )}
     </div>
   );
