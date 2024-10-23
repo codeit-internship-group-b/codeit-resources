343f21d48a6b6cf1925d01de2fbe800e34810d75
YoungJun Bae <dudwns0213@gmail.com>
2024-10-23 14:12:50 +0900

    FE-Feat: mockdata(json, ts) 생성

---
+++ apps/web/app/mocks/mockData.ts
@@ -0,0 +1,18 @@
+// mockData.ts
+export const rooms = [
+  {
+    id: "roomA",
+    title: "회의실 A",
+    schedules: [
+      {
+        id: "scheduleA1",
+        date: "2024-10-16",
+        start_time: "09:00",
+        end_time: "09:30",
+        title: "팀 미팅",
+      },
+      // more schedules...
+    ],
+  },
+  // more rooms...
+];
