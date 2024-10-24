c3e1c241731305bf1742af17be68ff98882ed135
Lin <heron6534@gmail.com>
2024-10-24 16:30:13 +0900

    FE-Feat: Radio Group 컴포넌트에 value prop을 추가하고 내부 상태 관리 로직 수정

--- packages/ui/src/types/RadioButtonTypes.ts
+++ packages/ui/src/types/RadioButtonTypes.ts
@@ -1,9 +1,9 @@
- 
 import { type ReactNode } from "react";
 
 export interface RadioGroupProps {
   children: ReactNode;
   defaultValue?: string;
+  value?: string;
   onChange?: (value: string) => void;
   legend?: string;
 }
