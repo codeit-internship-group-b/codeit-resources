db53fcbd47ca3629652cb2f7c479d057b8b4711f
김보경 <rlaqhrud6263@naver.com>
2024-10-25 17:12:52 +0900

    BE-Chore: 스프레드문법 Error로 인한 dev pull 진행용 commit

--- apps/api/src/middleware/authenticateToken.ts
+++ apps/api/src/middleware/authenticateToken.ts
@@ -1,6 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
 import { type JwtPayload, verify, type Secret } from "jsonwebtoken";
 import { config } from "dotenv";
+import { type IUser } from "@repo/types/userType";
 import { User } from "../models/userModel";
 
 config();
@@ -9,6 +10,7 @@ const JWT_SECRET = process.env.JWT_SECRET as Secret;
 
 interface AuthenticateTokenRequest extends Request {
   token: string;
+  user?: IUser;
 }
 
 type CustomJwtPayload = JwtPayload & { id: string };
@@ -19,35 +21,38 @@ export const authenticateToken = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
+    const authHeader = req.header("Authorization");
+    const token = authHeader?.replace("Bearer ", "");
+
+    if (!authHeader?.startsWith("Bearer ")) {
+      res.status(400).send({ message: "유효하지 않은 인증 헤더입니다." });
+      return;
+    }
 
     if (!token) {
-      throw new Error("토큰이 존재하지 않습니다.");
+      res.status(400).send({ message: "토큰이 존재하지 않습니다." });
+      return;
     }
 
     const decodedToken = verify(token, JWT_SECRET) as CustomJwtPayload;
     const userId = decodedToken.id;
 
     if (!userId) {
-      throw new Error("잘못된 토큰입니다.");
+      res.status(400).send({ message: "잘못된 토큰입니다." });
+      return;
     }
-
-    console.log(decodedToken);
 
     const user = await User.findById(userId);
 
     if (!user) {
-      res.clearCookie("authorization");
-      throw new Error("사용자가 존재하지 않습니다.");
+      res.status(404).send({ message: "사용자가 존재하지 않습니다." });
+      return;
     }
+
+    // req.user = { ...user, _id: userId } as IUser;
 
     next();
   } catch (error) {
-    // const err = error as Error;
-    // res.clearCookie("authorization");
-    // res.status(401).send({ message: err.message });
-
-    res.clearCookie("authorization");
     next(error);
   }
 };
