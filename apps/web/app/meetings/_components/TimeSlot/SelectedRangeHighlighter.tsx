efc26d0c4df1056f2f03d37819f3c25c00c5aa14
YoungJun Bae <dudwns0213@gmail.com>
2024-10-26 16:42:44 +0900

    FE-Remove: 시간표 관련 컴포넌트 삭제(재구현을 위해)

--- apps/web/app/meetings/_components/TimeSlot/SelectedRangeHighlighter.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface SelectedRangeHighlighterProps {
-  isInSelectedRange: boolean;
-}
-
-export const SelectedRangeHighlighter: React.FC<SelectedRangeHighlighterProps> = ({ isInSelectedRange }) => {
-  return <div className={isInSelectedRange ? "bg-blue-200" : ""} />;
-};
