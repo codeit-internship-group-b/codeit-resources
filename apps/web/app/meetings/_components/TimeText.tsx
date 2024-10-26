680771a86effec1d25fd040f5f957013a9f9d3ca
YoungJun Bae <dudwns0213@gmail.com>
2024-10-26 17:02:56 +0900

    FE-Feat: TimeHeader 삭제 및 Text로 교체

--- apps/web/app/meetings/_components/TimeText.tsx
+++ apps/web/app/meetings/_components/TimeText.tsx
@@ -1,22 +1,25 @@
-// components/TimeList.tsx
+// components/TimeText.tsx
 
 import React from "react";
 
-const TimeList: React.FC = () => {
-  const times = Array.from({ length: 25 }, (_, index) => {
-    const hour = index.toString().padStart(2, "0");
-    return `${hour}:00`;
+const TimeText: React.FC = () => {
+  const times = Array.from({ length: 49 }, (_, index) => {
+    if (index % 2 === 0) {
+      const hour = (index / 2).toString().padStart(2, "0");
+      return `${hour}:00`;
+    }
+    return "";
   });
 
   return (
-    <div className="flex">
-      {times.map((time) => (
-        <div key={time} className="py-1">
-          {time}
+    <div className="flex w-full">
+      {times.map((time, index) => (
+        <div key={index} className="flex min-w-72 items-center justify-center">
+          {time ? <span className="text-custom-black/50 text-xs-semibold">{time}</span> : null}
         </div>
       ))}
     </div>
   );
 };
 
-export default TimeList;
+export default TimeText;
