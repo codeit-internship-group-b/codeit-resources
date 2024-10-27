30425db83eaed648202a9f9211b8bc9ebc8c5237
cindyCho <hjcho2303@naver.com>
2024-10-27 22:19:20 +0900

    FE-Fix: headerTab index 바뀌는 것 수정
    Feat: 오늘 날짜에 뱃지 붙이기

--- apps/web/components/Header/HeaderTabs.tsx
+++ apps/web/components/Header/HeaderTabs.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import cn from "@ui/src/utils/cn";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { formatDate, getDatesForSeats, getDatesFromTodayToEndOfMonth } from "@ui/src/utils/date";
 import { useDateStore } from "@/app/store/useDateStore";
@@ -28,45 +28,60 @@ export default function HeaderTabs({ page }: HeaderProps): JSX.Element {
     }
   };
 
+  useEffect(() => {
+    setActiveTabIndex(0);
+  }, [selectedDate.month]);
+
+  const today = new Date();
+  const isCurrentMonth = selectedDate.year === today.getFullYear() && selectedDate.month === today.getMonth() + 1;
+
   return (
-    <div className="h-45 scrollbar-hidden relative top-4 flex items-end gap-24 overflow-auto md:static">
-      {dates.map((date, index) => (
-        <motion.div
-          key={String(date)}
-          className="flex min-w-fit cursor-pointer flex-col gap-16"
-          onClick={() => {
-            handleActiveTab(index);
-          }}
-          role="tab"
-          aria-selected={activeTabIndex === index}
-          tabIndex={0}
-          whileHover={activeTabIndex !== index ? { scale: 0.96 } : {}}
-          transition={{ duration: 0.2 }}
-        >
-          <motion.span
-            className={cn({
-              "text-text-default": activeTabIndex !== index,
-              "text-text-inverse font-bold": activeTabIndex === index,
-            })}
-            animate={{
-              color: activeTabIndex === index ? "#6500C2" : "#c3c7cc",
+    <>
+      {isCurrentMonth ? (
+        <span className="text-10 rounded-8 absolute bg-[#EB008D] px-4 py-2 text-center font-medium text-white">
+          오늘
+        </span>
+      ) : null}
+
+      <div className="scrollbar-hidden h-69 relative top-4 flex items-end gap-24 overflow-auto md:static">
+        {dates.map((date, index) => (
+          <motion.div
+            key={String(date)}
+            className="flex min-w-fit cursor-pointer flex-col gap-16"
+            onClick={() => {
+              handleActiveTab(index);
             }}
-            whileHover={activeTabIndex !== index ? { color: "#6500C2" } : {}}
+            role="tab"
+            aria-selected={activeTabIndex === index}
+            tabIndex={0}
+            whileHover={activeTabIndex !== index ? { scale: 0.96 } : {}}
             transition={{ duration: 0.2 }}
           >
-            {formatDate(date, page)}
-          </motion.span>
-          <motion.span
-            className="w-full border-b-2 border-solid"
-            initial={{ scaleX: 0 }}
-            animate={{
-              scaleX: activeTabIndex === index ? 1 : 0,
-              borderColor: activeTabIndex === index ? "#6500C2" : "rgba(255, 255, 255, 0)",
-            }}
-            transition={{ duration: 0.3 }}
-          />
-        </motion.div>
-      ))}
-    </div>
+            <motion.span
+              className={cn({
+                "text-text-default": activeTabIndex !== index,
+                "text-text-inverse font-bold": activeTabIndex === index,
+              })}
+              animate={{
+                color: activeTabIndex === index ? "#6500C2" : "#c3c7cc",
+              }}
+              whileHover={activeTabIndex !== index ? { color: "#6500C2" } : {}}
+              transition={{ duration: 0.2 }}
+            >
+              {formatDate(date, page)}
+            </motion.span>
+            <motion.span
+              className="w-full border-b-2 border-solid"
+              initial={{ scaleX: 0 }}
+              animate={{
+                scaleX: activeTabIndex === index ? 1 : 0,
+                borderColor: activeTabIndex === index ? "#6500C2" : "rgba(255, 255, 255, 0)",
+              }}
+              transition={{ duration: 0.3 }}
+            />
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
