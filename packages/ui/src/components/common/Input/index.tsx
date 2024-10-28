8b6165f9da614d72a325eedd0ba267ef0ad83315
cindyCho <hjcho2303@naver.com>
2024-10-28 13:36:39 +0900

    Refactor: 코드리뷰에 맞게 수정

--- packages/ui/src/components/common/Input/index.tsx
+++ packages/ui/src/components/common/Input/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import cn from "@ui/src/utils/cn";
-import React, { InputHTMLAttributes, useState, ChangeEvent, useEffect, forwardRef } from "react";
+import { InputHTMLAttributes, useState, ChangeEvent, useEffect, forwardRef } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 import ErrorMessage from "../ErrorMessage";
 
@@ -16,84 +16,66 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   value?: string;
 }
 
-const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
-  {
-    placeholder,
-    id,
-    type = "text",
-    isError,
-    errorMessage,
-    className,
-    value = "",
-    register = {
-      onChange: async () => {},
-      onBlur: async () => {},
-      ref: (e: HTMLInputElement | null) => {
-        e;
-      },
-      name: "",
-    },
-    ...args
-  },
-  ref,
-) {
-  const { onChange, onBlur, disabled, name } = register;
-  const [hasValue, setHasValue] = useState(!!value);
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, id, type = "text", isError, errorMessage, className, value = "", register, ...args }, ref) => {
+    const { onChange = () => {}, onBlur = () => {}, disabled, name, ref: regRef } = register || {};
+    const [hasValue, setHasValue] = useState(!!value);
 
-  useEffect(() => {
-    setHasValue(!!value);
-  }, [value]);
+    useEffect(() => {
+      setHasValue(!!value);
+    }, [value]);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setHasValue(!!e.target.value);
-    onChange(e);
-  };
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setHasValue(!!e.target.value);
+      onChange(e);
+    };
 
-  return (
-    <div className={cn("group relative", className)}>
-      <input
-        id={id}
-        defaultValue={value}
-        name={name || id}
-        type={type}
-        className={cn(
-          "transition-linear border-custom-black/40 hover:bg-custom-black/5 peer w-full rounded-lg border border-solid p-14 placeholder-transparent focus:hover:bg-purple-700/5",
-          {
-            "border-error focus:hover:bg-custom-black/5": isError,
-            "focus:border-purple-400": !isError,
-          },
-        )}
-        style={{ outline: "none" }}
-        placeholder={placeholder}
-        onBlur={onBlur}
-        onChange={handleChange}
-        disabled={disabled}
-        ref={ref || register.ref}
-        {...args}
-      />
-      <label
-        htmlFor={id}
-        className={cn(
-          "bottom-39 transition-linear text-custom-black/80 peer-focus:-translate-y-27 peer-focus:!text-13 relative left-16 z-10 bg-transparent p-0 leading-none peer-placeholder-shown:translate-y-0 peer-focus:bg-white peer-focus:px-3",
-          {
-            "peer-focus:bg-transparent peer-focus:text-purple-400": !isError,
-            "peer-focus:text-error peer-focus:bg-transparent": isError,
-            "-translate-y-27 !text-13 bg-transparent px-3": !isError && hasValue,
-            "-translate-y-27 !text-13 text-error bg-transparent px-3": isError && hasValue,
-          },
-        )}
-        style={{ display: "inline-block" }}
-      >
-        <span className="relative z-10">{placeholder}</span>
-        <span
-          className={cn("absolute bottom-6 left-0 right-0 z-0 h-4 group-focus-within:bg-white", {
-            "bottom-6 bg-white": hasValue,
-          })}
+    return (
+      <div className={cn("group relative", className)}>
+        <input
+          id={id}
+          defaultValue={value}
+          name={name || id}
+          type={type}
+          className={cn(
+            "transition-linear border-custom-black/40 hover:bg-custom-black/5 peer w-full rounded-lg border border-solid p-14 placeholder-transparent focus:hover:bg-purple-700/5",
+            {
+              "border-error focus:hover:bg-custom-black/5": isError,
+              "focus:border-purple-400": !isError,
+            },
+          )}
+          style={{ outline: "none" }}
+          placeholder={placeholder}
+          onBlur={onBlur}
+          onChange={handleChange}
+          disabled={disabled}
+          ref={ref || regRef}
+          {...args}
         />
-      </label>
-      {isError && <ErrorMessage className="-bottom-6 left-20" message={errorMessage} />}
-    </div>
-  );
-});
+        <label
+          htmlFor={id}
+          className={cn(
+            "bottom-39 transition-linear text-custom-black/80 peer-focus:-translate-y-27 peer-focus:!text-13 relative left-16 z-10 bg-transparent p-0 leading-none peer-placeholder-shown:translate-y-0 peer-focus:bg-white peer-focus:px-3",
+            {
+              "peer-focus:bg-transparent peer-focus:text-purple-400": !isError,
+              "peer-focus:text-error peer-focus:bg-transparent": isError,
+              "-translate-y-27 !text-13 bg-transparent px-3": !isError && hasValue,
+              "-translate-y-27 !text-13 text-error bg-transparent px-3": isError && hasValue,
+            },
+          )}
+          style={{ display: "inline-block" }}
+        >
+          <span className="relative z-10">{placeholder}</span>
+          <span
+            className={cn("absolute bottom-6 left-0 right-0 z-0 h-4 group-focus-within:bg-white", {
+              "bottom-6 bg-white": hasValue,
+            })}
+          />
+        </label>
+        {isError && <ErrorMessage className="-bottom-6 left-20" message={errorMessage} />}
+      </div>
+    );
+  },
+);
 
 export default Input;
