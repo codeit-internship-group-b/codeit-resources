ffdb8831012219e2a38af79b6340f4315bae5c62
AdamSeungheonShin <ssh9809@naver.com>
2024-10-28 15:19:02 +0900

    FE-Feat: 카테고리명 수정 input 구현

--- apps/web/app/admin/(items)/_components/CategoryListItem.tsx
+++ apps/web/app/admin/(items)/_components/CategoryListItem.tsx
@@ -1,7 +1,44 @@
 "use client";
 
 import ListItem from "@ui/src/components/common/ListItem";
+import { useOnClickOutside } from "@ui/src/hooks/useOnClickOutside";
+import { useRef, useState } from "react";
+
+const MOCK_TITLE = "카테고리 제목";
 
 export default function CategoryListItem(): JSX.Element {
-  return <ListItem>카테고리 아이템</ListItem>;
+  const [isModify, setIsModify] = useState(false);
+  const [changeName, setChangeName] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOnClickOutside(inputRef, () => {
+    if (isModify) {
+      setIsModify(false);
+    }
+  });
+  return (
+    <ListItem>
+      <span className="flex flex-grow items-center gap-32 text-left">
+        {isModify ? (
+          <input
+            ref={inputRef}
+            placeholder="팀 이름"
+            className="placeholder:text-custom-black/50 w-full placeholder:underline placeholder:underline-offset-4 focus:outline-none"
+            onChange={(e) => {
+              setChangeName(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                // TODO: input 데이터 patch
+                // eslint-disable-next-line no-console
+                console.log(changeName);
+              }
+            }}
+          />
+        ) : (
+          MOCK_TITLE
+        )}
+      </span>
+    </ListItem>
+  );
 }
