e462eb802978f3bec1b1ea60c238f77d87ab9795
YoungJun Bae <dudwns0213@gmail.com>
2024-10-28 13:19:21 +0900

    FE-Feat: 모바일일때 화면, 데스크탑일 때 화면 구분

--- apps/web/app/meetings/_components/ScheduleTable.tsx
+++ apps/web/app/meetings/_components/ScheduleTable.tsx
@@ -2,8 +2,9 @@
 
 import React from "react";
 import ScheduleRow from "./Schedule/ScheduleRow";
-import TimeText from "./Schedule/ScheduleRow/TimeText";
 import RoomName from "./Schedule/RoomName";
+import CurrentTimeIndicator from "./Schedule/ScheduleRow/CurrentTimeIndicator";
+import TimeText from "./Schedule/ScheduleRow/TimeText";
 
 interface Schedule {
   id: string;
@@ -27,18 +28,49 @@ interface ScheduleTableProps {
 
 const ScheduleTable: React.FC<ScheduleTableProps> = ({ rooms, selectedDate }) => {
   return (
-    <div>
+    <div className="w-full">
+      {/* 모바일 레이아웃 */}
       {rooms.map((room) => (
-        <>
-          <RoomName key={room.title} name={room.title} />
-          <div key={room.id} className="my-24 overflow-x-auto">
+        <div key={room.id} className="block md:hidden">
+          <RoomName name={room.title} />
+          <div className="my-24 h-auto overflow-x-auto">
             <TimeText />
-            <div className="ml-36">
+            <div className="mb-30 ml-36">
               <ScheduleRow schedules={room.schedules.filter((schedule) => schedule.date === selectedDate)} />
             </div>
           </div>
-        </>
+        </div>
       ))}
+
+      {/* 데스크탑 레이아웃 */}
+      <div className="hidden md:block">
+        <div className="flex">
+          {/* 미팅룸 이름 영역 */}
+          <div className="my-34 w-1/8 ml-30 flex flex-col justify-between border-r border-gray-200">
+            {rooms.map((room) => (
+              <RoomName key={room.id} name={room.title} />
+            ))}
+          </div>
+          {/* 시간표 영역 */}
+          <div className="relative w-3/4 overflow-x-auto">
+            {/* 시간 라벨 */}
+            <TimeText />
+
+            {/* 스케줄 리스트 */}
+            {rooms.map((room) => (
+              <ScheduleRow
+                key={room.id}
+                schedules={room.schedules.filter((schedule) => schedule.date === selectedDate)}
+                slotWidth={72} // 필요에 따라 조정
+                slotHeight={80} // 필요에 따라 조정
+              />
+            ))}
+
+            {/* 현재 시간 표시 */}
+            <CurrentTimeIndicator slotWidth={72} startHour={0} endHour={24} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
