53adac3b3d9b0177e5dbeb18d515a2f1727adc47
cindyCho <hjcho2303@naver.com>
2024-10-29 23:49:11 +0900

    FE-Refactor: loading bar props에 맞게 수정

--- apps/web/components/common/Skeleton/ListItemSkeleton.tsx
+++ apps/web/components/common/Skeleton/ListItemSkeleton.tsx
@@ -25,10 +25,10 @@ function ListItemSkeleton({ type = "team", thickness = "thick", color = "white" 
         {type === "member" && (
           <>
             <PersonEmptyIcon />
-            <LoadingBar width="w-44" />
+            <LoadingBar classNames="w-44 h-12" />
           </>
         )}
-        <LoadingBar width={type === "team" ? "w-100" : "w-200"} />
+        <LoadingBar classNames={type === "team" ? "w-100" : "w-200"} />
       </div>
     </ListItem>
   );
