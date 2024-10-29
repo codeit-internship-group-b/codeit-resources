4eca4750ce972ea9ea42c4780db3f5f7649654e4
cindyCho <hjcho2303@naver.com>
2024-10-29 23:47:02 +0900

    FE-Style: 로딩상태 모바일 버전 수정

--- apps/web/app/dashboard/_components/DashboardSectionLoading.tsx
+++ apps/web/app/dashboard/_components/DashboardSectionLoading.tsx
@@ -5,12 +5,12 @@ export default function DashboardSectionLoading(): JSX.Element {
     <div>
       <h1 className="mb-16">내 회의</h1>
       <hr className="mb-16 border-gray-200/10" />
-      <div className="scrollbar-hidden flex gap-16 overflow-auto">
+      <div className="scrollbar-hidden overflow-auto md:flex md:gap-16">
         {Array.from({ length: 3 }).map((_, index) => (
           <div
             // eslint-disable-next-line react/no-array-index-key
             key={index}
-            className="rounded-8 text-custom-black/80 min-w-259 w-259 h-148 md:w-275 md:h-172 mb-4 flex flex-col gap-16 border border-solid border-gray-200/10 p-32"
+            className="rounded-8 text-custom-black/80 h-123 md:w-275 md:h-172 relative mb-16 flex w-full flex-col gap-4 border border-solid border-gray-200/10 p-20 md:gap-8 md:p-32"
           >
             <LoadingBar classNames="h-20 w-100" />
             <LoadingBar classNames="h-12 w-80" />
