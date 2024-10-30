978e23aa4b6166f4f88c5571fe4033b56337a826
AdamSeungheonShin <ssh9809@naver.com>
2024-10-30 14:45:05 +0900

    Refactor: 예약 조회 시 item 필드 ref 추가

--- apps/api/src/controllers/reservationControllers.ts
+++ apps/api/src/controllers/reservationControllers.ts
@@ -103,7 +103,7 @@ export const getReservationsByTypeAndDate = async (
   const reservations: IReservation[] = await Reservation.find(query)
     .populate("user", "name email")
     .populate("attendees", "name email")
-    .populate({ path: "item", select: "name", model: itemTypeToModel[itemType] })
+    .populate("item", "name itemType")
     .sort({ status: 1, startAt: 1 });
 
   res.status(200).json(reservations);
