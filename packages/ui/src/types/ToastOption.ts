0091f590d1e94fd2f2ca224e7cfe05312ceb01c1
Adam Shin <137388618+AdamSeungheonShin@users.noreply.github.com>
2024-10-30 16:49:45 +0900

    Refactor: toast option autoclose 시간 수정

--- packages/ui/src/types/ToastOption.ts
+++ packages/ui/src/types/ToastOption.ts
@@ -2,7 +2,7 @@ import type { ToastOptions } from "react-toastify";
 
 export const CustomToastOptions: ToastOptions = {
   position: "top-center",
-  autoClose: 3000,
+  autoClose: 1000,
   hideProgressBar: true,
   closeOnClick: true,
   pauseOnHover: true,
