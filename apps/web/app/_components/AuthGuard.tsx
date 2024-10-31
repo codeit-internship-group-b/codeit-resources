c0343ec1108ce32cd422511054e891bfb654c6c1
김보경 <rlaqhrud6263@naver.com>
2024-10-31 21:00:15 +0900

    FE-Feat: AuthGuard로 로그인 페이지 접근 제한

--- apps/web/app/_components/AuthGuard.tsx
+++ apps/web/app/_components/AuthGuard.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { PAGE_NAME } from "@ui/src/utils/constants/pageNames";
+import { type AuthStore } from "@repo/types";
+import { useEffect } from "react";
+import { getLocalStorage } from "../_utils/getLocalStorage";
 import SignInForm from "./SignInForm";
 
-export default function AuthGuard(): JSX.Element {
-  return <SignInForm />;
+export default function AuthGuard(): JSX.Element | null {
+  const router = useRouter();
+  const userStorage = getLocalStorage<AuthStore>("userResponseStore");
+
+  const { isLoggedIn } = userStorage ?? { isLoggedIn: false };
+
+  useEffect(() => {
+    if (isLoggedIn) router.replace(PAGE_NAME.DASHBOARD);
+  }, [isLoggedIn, router]);
+
+  return isLoggedIn ? null : <SignInForm />;
 }
