beb3c72cdf6019b8e22ed6bf3d90d2efe6db311d
AdamSeungheonShin <ssh9809@naver.com>
2024-11-03 19:23:52 +0900

    FE-Feat: 회의실 하위 아이템 컴포넌트 UI 구현

--- apps/web/app/admin/(items)/_components/CategoryList.tsx
+++ apps/web/app/admin/(items)/_components/CategoryList.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import CategoryListItem from "./CategoryListItem";
 
 export default function CategoryList(): JSX.Element {
   return (
     <div>
-      <CategoryListItem />
+      {/* todo 데이터 받아서 mapping */}
+      <CategoryListItem title="회의실" />
     </div>
   );
 }
--- apps/web/app/admin/(items)/_components/CategoryListItem.tsx
+++ apps/web/app/admin/(items)/_components/CategoryListItem.tsx
@@ -3,14 +3,19 @@
 import ListItem from "@ui/src/components/common/ListItem";
 import { useOnClickOutside } from "@ui/src/hooks/useOnClickOutside";
 import { useRef, useState } from "react";
+import { motion } from "framer-motion";
 import CategoryEditDropdown from "./CategoryEditDropdown";
 import ConfirmationModal from "./ConfirmationModal";
+import CategoryListSubItem from "./CategoryListSubItem";
 
-const MOCK_TITLE = "회의실";
+interface CategoryListItemProps {
+  title: string;
+}
 
-export default function CategoryListItem(): JSX.Element {
+export default function CategoryListItem({ title }: CategoryListItemProps): JSX.Element {
   const [isModifying, setIsModifying] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
   useOnClickOutside(inputRef, () => {
@@ -19,32 +24,49 @@ export default function CategoryListItem(): JSX.Element {
     }
   });
 
+  const toggleListItem = (): void => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <ListItem>
-      <span className="flex flex-grow items-center gap-32 text-left">
-        {isModifying ? (
-          <input
-            ref={inputRef}
-            placeholder="카테고리"
-            className="placeholder:text-custom-black/50 w-full placeholder:underline placeholder:underline-offset-4 focus:outline-none"
-            onChange={(e) => {
-              setInputValue(e.target.value);
-            }}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                // TODO: input 데이터 patch
-                // eslint-disable-next-line no-console
-                console.log(inputValue);
-              }
-            }}
-          />
-        ) : (
-          MOCK_TITLE
-        )}
-      </span>
-      <ConfirmationModal Title={MOCK_TITLE}>
-        <CategoryEditDropdown isModifying={isModifying} setIsModifying={setIsModifying} />
-      </ConfirmationModal>
-    </ListItem>
+    <>
+      <ListItem color="gray" thickness="thick" onClick={toggleListItem}>
+        <span className="flex flex-grow items-center gap-32 text-left">
+          {isModifying ? (
+            <input
+              ref={inputRef}
+              placeholder="카테고리"
+              className="placeholder:text-custom-black/50 bg-gray-60 w-full placeholder:underline placeholder:underline-offset-4 focus:outline-none"
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  // TODO: input 데이터 patch
+                  // eslint-disable-next-line no-console
+                  console.log(inputValue);
+                }
+              }}
+            />
+          ) : (
+            title
+          )}
+        </span>
+        <ConfirmationModal Title={title}>
+          <CategoryEditDropdown isModifying={isModifying} setIsModifying={setIsModifying} />
+        </ConfirmationModal>
+      </ListItem>
+      {isOpen ? (
+        <motion.div
+          initial={{ opacity: 0, height: 0 }}
+          animate={{ opacity: 1, height: "auto" }}
+          exit={{ opacity: 0, height: 0 }}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+          className="pl-24"
+        >
+          <CategoryListSubItem title="회의실1" />
+        </motion.div>
+      ) : null}
+    </>
   );
 }
