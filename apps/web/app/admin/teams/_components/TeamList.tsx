3b7142002acd03581314aad03a8093067566ab6c
김보경 <rlaqhrud6263@naver.com>
2024-11-07 17:11:03 +0900

    BE-Chore: null 반환 삭제

--- apps/web/app/admin/teams/_components/TeamList.tsx
+++ apps/web/app/admin/teams/_components/TeamList.tsx
@@ -3,12 +3,11 @@
 import { useTeams } from "../_hooks/useTeams";
 import TeamListItem from "./TeamListItem";
 
-export default function TeamList(): JSX.Element | null {
+export default function TeamList(): JSX.Element {
   const { data: teams } = useTeams();
 
   return (
     <div className="mt-40">
-      {teams.length === 0 && null}
       {teams.map((team) => (
         <TeamListItem key={team.name} team={team} />
       ))}
