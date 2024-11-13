1651e35ab39bbd2d94b55516633db77753f6b37b
Lin <heron6534@gmail.com>
2024-11-13 22:39:41 +0900

    FE-Chore: Skeleton 관련 파일 Skeleton 폴더 하위로 이동

--- apps/web/app/admin/members/_components/skeleton/MemberListSkeleton.tsx
+++ apps/web/app/admin/members/_components/skeleton/MemberListSkeleton.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable react/no-array-index-key */
 import ListItemSkeleton from "@/components/common/Skeleton/ListItemSkeleton";
 
-export default function SkeletonList(): JSX.Element {
+export default function MemberListSkeleton(): JSX.Element {
   return (
     <div className="flex flex-col gap-16">
       {Array.from({ length: 8 }).map((_, index) => (
+        // index 이외 고유 key값 X
+        // eslint-disable-next-line react/no-array-index-key
         <ListItemSkeleton key={index} type="member" thickness="thick" color="white" showHamburger={false} />
       ))}
     </div>
---
+++ apps/web/app/admin/members/_components/skeleton/TabSkeleton.tsx
@@ -0,0 +1,18 @@
+import LoadingBar from "@/components/common/Skeleton/LoadingBar";
+
+export default function TabsSkeleton(): JSX.Element {
+  return (
+    <>
+      <ul className="mb-16 flex flex-row gap-32 whitespace-nowrap">
+        {Array.from({ length: 3 }).map((_, index) => (
+          // index 이외 고유 key값 X
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={index} className="py-8">
+            <LoadingBar classNames="w-60 h-12" />
+          </li>
+        ))}
+      </ul>
+      <div className="mb-24 w-full border border-gray-200/10" />
+    </>
+  );
+}
