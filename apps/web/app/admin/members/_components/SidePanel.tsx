5649b00cfb433a3f76b1e5df1eb3761fb5bc87ce
Lin <heron6534@gmail.com>
2024-11-13 22:49:25 +0900

    FE-Chore: boder 색상 수정

--- apps/web/app/admin/members/_components/SidePanel.tsx
+++ apps/web/app/admin/members/_components/SidePanel.tsx
@@ -132,7 +132,7 @@ export default function SidePanel({ isOpen, onClose, selectedMember }: AddMember
   return (
     <div
       ref={sidePanelRef}
-      className={`md:w-414 border-custom-black fixed right-0 top-0 z-10 h-full w-full transform border-l bg-white shadow-[0px_2px_14px_0px_rgba(0,0,0,0.08)] transition-transform duration-300 ease-in-out ${
+      className={`md:w-414 border-custom-black/20 fixed right-0 top-0 z-10 h-full w-full transform border-l bg-white shadow-[0px_2px_14px_0px_rgba(0,0,0,0.08)] transition-transform duration-300 ease-in-out ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
