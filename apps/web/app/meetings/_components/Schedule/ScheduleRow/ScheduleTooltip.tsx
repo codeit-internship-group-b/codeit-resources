89256a22f9f765fda2623644250dfcba37b0a744
YoungJun Bae <dudwns0213@gmail.com>
2024-11-14 16:47:25 +0900

    Feat: Tooltip props children으로 변경

--- apps/web/app/meetings/_components/Schedule/ScheduleRow/ScheduleTooltip.tsx
+++ apps/web/app/meetings/_components/Schedule/ScheduleRow/ScheduleTooltip.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import { type ReactNode } from "react";
+
 interface ScheduleTooltipProps {
-  title: string;
+  children?: ReactNode;
 }
 
 export default function ScheduleTooltip(props: ScheduleTooltipProps): JSX.Element {
-  const { title } = props;
+  const { children } = props;
   return (
     <div className="absolute bottom-full left-1/2 mb-14 hidden w-max -translate-x-1/2 transform group-hover:block">
       <div className="text-sm-medium bg-gray-90 relative z-10 rounded-lg px-8 py-4 text-sm text-white/90">
-        {title}
+        {children}
         <div className="border-t-gray-90 absolute left-20 top-full h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-x-transparent" />
       </div>
     </div>
