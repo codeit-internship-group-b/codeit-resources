b699d6a0bdf3e4f64e3b0637bf7c67003a59f825
YoungJun Bae <dudwns0213@gmail.com>
2024-11-14 16:43:52 +0900

    Feat: ScheduleTableMobile api 연동

--- apps/web/app/meetings/_components/Schedule/ScheduleTable/ScheduleTableMobile.tsx
+++ apps/web/app/meetings/_components/Schedule/ScheduleTable/ScheduleTableMobile.tsx
@@ -1,31 +1,49 @@
 "use client";
 
-import { type ScheduleDate } from "@/app/types/scheduletypes";
+import { type TBaseItem } from "@repo/types";
+import { type IReservation } from "@repo/types/src/reservationType";
 import RoomName from "../RoomName";
 import ScheduleRow from "../ScheduleRow";
 import TimeText from "../ScheduleRow/TimeText";
 
-type ScheduleTableMobileProps = ScheduleDate;
+interface ScheduleTableMobileProps {
+  rooms: TBaseItem[];
+  meetingsData: IReservation[];
+  selectedDate: string;
+}
 
 export default function ScheduleTableMobile(props: ScheduleTableMobileProps): JSX.Element {
-  const { rooms, selectedDate } = props;
+  const { rooms, meetingsData, selectedDate } = props;
 
   return (
     <div className="mx-16 my-24 block w-full md:hidden">
-      {rooms.map((room) => (
-        <div key={room.id} className="mb-26">
-          <RoomName name={room.title} />
-          <div className="mt-30 no-scrollbar overflow-x-auto pb-20">
-            <TimeText />
-            <div className="ml-36 mt-8">
-              <ScheduleRow
-                schedules={room.schedules.filter((schedule) => schedule.date === selectedDate)}
-                room={room.title}
-              />
+      {rooms.map((room) => {
+        // 해당 방의 스케줄 필터링
+        const roomSchedules = meetingsData.filter((schedule) => {
+          const scheduleItemId = typeof schedule.item === "string" ? schedule.item : schedule.item._id;
+
+          // 방 ID가 일치하는지 확인
+          const isSameRoom = scheduleItemId === room._id;
+
+          // 예약 날짜가 selectedDate와 같은지 확인
+          const scheduleDate = new Date(schedule.startAt).toISOString().split("T")[0];
+          const isSameDate = scheduleDate === selectedDate;
+
+          return isSameRoom && isSameDate;
+        });
+
+        return (
+          <div key={room._id} className="mb-26">
+            <RoomName name={room.name} />
+            <div className="mt-30 no-scrollbar overflow-x-auto pb-20">
+              <TimeText />
+              <div className="ml-36 mt-8">
+                <ScheduleRow schedules={roomSchedules} room={room.name} />
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
