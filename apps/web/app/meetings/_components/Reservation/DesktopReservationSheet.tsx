1bff89bb64e401bdccc95da7d8ffecde4a2a8847
YoungJun Bae <dudwns0213@gmail.com>
2024-11-18 19:18:54 +0900

    Feat: createReservationMutation 구현 및 Form을 통해 예약되는 기능 구현

--- apps/web/app/meetings/_components/Reservation/DesktopReservationSheet.tsx
+++ apps/web/app/meetings/_components/Reservation/DesktopReservationSheet.tsx
@@ -1,11 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { useEffect, useState } from "react";
 import { type IReservation } from "@repo/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { notify } from "@ui/index";
 import Sidebar from "@/components/common/Sidebar";
 import { type ScheduleFormData, type Schedule, type SelectedRoom } from "@/app/types/scheduletypes";
 import { useSidebarStore } from "@/app/store/useSidebarStore";
+import { createReservation, type CreateReservationRequest } from "@/api/reservations";
 import ReservationForm from "./ReservationForm";
 import ReservationModal from "./ReservationModal";
 
@@ -18,12 +20,32 @@ interface DesktopReservationSheetProps {
 
 export default function DesktopReservationSheet(props: DesktopReservationSheetProps): JSX.Element {
   const { onClose, selectedTime, selectedSchedule, selectedRoom } = props;
-
+  const queryClient = useQueryClient();
   const { isSidebarOpen, closeSidebar } = useSidebarStore();
-
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleSubmit = (data: ScheduleFormData): void => {
+  const [formData, setFormData] = useState<{ data: CreateReservationRequest; itemId: string } | null>(null);
+
+  // Reservation mutation 설정
+  const createReservationMutation = useMutation({
+    mutationFn: (formData: { data: CreateReservationRequest; itemId: string }) => {
+      // ScheduleFormData를 CreateReservationRequest 형식으로 변환
+
+      return createReservation(formData.itemId, formData.data);
+    },
+    onSuccess: async () => {
+      notify({
+        type: "success",
+        message: "회의실 예약이 다다다다다.",
+      });
+      await queryClient.invalidateQueries({ queryKey: ["reservation"] });
+      setIsModalOpen(false);
+      onClose();
+    },
+  });
+
+  const handleSubmit = (data: CreateReservationRequest, itemId: string): void => {
+    setFormData({ data, itemId });
     setIsModalOpen(true);
   };
 
@@ -55,6 +77,9 @@ export default function DesktopReservationSheet(props: DesktopReservationSheetPr
           setIsModalOpen(false);
         }}
         onConfirm={() => {
+          if (formData) {
+            createReservationMutation.mutate(formData);
+          }
           setIsModalOpen(false);
           onClose();
         }}
