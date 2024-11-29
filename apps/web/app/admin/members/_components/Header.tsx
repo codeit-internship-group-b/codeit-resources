cd3cb70d7de46a55293500336232d84a862681b7
Lin <heron6534@gmail.com>
2024-11-29 16:45:23 +0900

    FE-Refactor: 조건부 렌더링을 반응형 스타일로 변경

--- apps/web/app/admin/members/_components/Header.tsx
+++ apps/web/app/admin/members/_components/Header.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@ui/index";
 import { Chevron } from "@ui/public";
-import useIsMobileStore from "@/app/store/useIsMobileStore";
 import SearchForm from "./SearchForm";
 
 interface HeaderProps {
@@ -10,34 +9,22 @@ interface HeaderProps {
 }
 
 export default function Header({ onMemberSelect, onSearch, keyword }: HeaderProps): JSX.Element {
-  const isMobile = useIsMobileStore();
-
   return (
-    <>
-      {isMobile ? (
-        <header className="my-16 flex items-center justify-between gap-20">
-          <Chevron />
-          <div className="h-54 relative w-full">
-            <SearchForm onSearch={onSearch} keyword={keyword} />
-          </div>
-        </header>
-      ) : (
-        <header className="mb-40 flex justify-between">
-          <h1 className="text-3xl-bold">멤버 관리</h1>
-          <div className="gap-30 flex justify-between">
-            <div className="h-42 relative w-[240px]">
-              <SearchForm onSearch={onSearch} keyword={keyword} />
-            </div>
-            <Button
-              onClick={onMemberSelect}
-              variant="Secondary"
-              className="w-122 h-42 text-lg-medium text-custom-black/80"
-            >
-              + 멤버 추가
-            </Button>
-          </div>
-        </header>
-      )}
-    </>
+    <header className="my-16 flex items-center justify-between gap-20 md:mb-40 md:mt-0 md:gap-0">
+      <Chevron className="md:hidden" />
+      <h1 className="text-3xl-bold hidden md:block">멤버 관리</h1>
+      <div className="md:gap-30 relative w-full md:flex md:w-auto">
+        <div className="h-54 md:h-42 relative md:w-[240px]">
+          <SearchForm onSearch={onSearch} keyword={keyword} />
+        </div>
+        <Button
+          onClick={onMemberSelect}
+          variant="Secondary"
+          className="w-122 h-42 text-lg-medium text-custom-black/80 hidden md:block"
+        >
+          + 멤버 추가
+        </Button>
+      </div>
+    </header>
   );
 }
--- apps/web/app/admin/members/_components/skeleton/index.tsx
+++ apps/web/app/admin/members/_components/skeleton/index.tsx
@@ -1,29 +1,19 @@
-"use client";
-
 import { Chevron, SearchIcon } from "@ui/public";
-import useIsMobileStore from "@/app/store/useIsMobileStore";
 import MemberListSkeleton from "./MemberListSkeleton";
 import TabsSkeleton from "./TabSkeleton";
 
 export default function Skeleton(): JSX.Element {
-  const isMobile = useIsMobileStore();
-
   return (
     <>
-      {isMobile ? (
-        <header className="my-16 flex items-center justify-between gap-20">
-          <Chevron className="animate-pulse" />
-          <div className="h-54 relative w-full">
-            <div className="rounded-100 flex h-full w-full items-center gap-14 bg-gray-100/30 px-20">
-              <SearchIcon />
-            </div>
+      <header className="my-16 flex items-center justify-between gap-20 md:mb-40 md:mt-0">
+        <Chevron className="animate-pulse md:hidden" />
+        <h1 className="text-3xl-bold hidden md:block">멤버 관리</h1>
+        <div className="h-54 relative w-full md:hidden">
+          <div className="rounded-100 flex h-full w-full items-center gap-14 bg-gray-100/30 px-20">
+            <SearchIcon />
           </div>
-        </header>
-      ) : (
-        <header className="mb-40 flex justify-between">
-          <h1 className="text-3xl-bold">멤버 관리</h1>
-        </header>
-      )}
+        </div>
+      </header>
       <TabsSkeleton />
       <MemberListSkeleton />
     </>
