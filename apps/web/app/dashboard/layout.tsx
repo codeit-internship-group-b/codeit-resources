b41d88175b98b96810adacf144d35fdcda3567b8
김보경 <rlaqhrud6263@naver.com>
2024-11-30 14:43:06 +0900

    Fix: children type 다시 롤백 => ReactNode Props

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
-import type { PropsWithChildren } from "react";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "대시보드 | Codeit Resources",
   description: "나의 회의 일정을 알 수 있는 페이지입니다.",
 };
 
-export default function DashboardLayout({ children }: PropsWithChildren): JSX.Element {
+export default function DashBoardLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>): JSX.Element {
   return <div className="md:ml-200">{children}</div>;
 }
--- apps/web/app/meetings/layout.tsx
+++ apps/web/app/meetings/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import type { PropsWithChildren } from "react";
+import type { ReactNode } from "react";
 import Header from "@/components/Header";
 
 export const metadata: Metadata = {
@@ -7,7 +7,11 @@ export const metadata: Metadata = {
   description: "회의 일정 및 관리를 위한 페이지입니다.",
 };
 
-export default function MeetingsLayout({ children }: PropsWithChildren): JSX.Element {
+export default function MeetingsLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>): JSX.Element {
   return (
     <div className="md:ml-200 mb-70">
       <Header page="meetings" />
