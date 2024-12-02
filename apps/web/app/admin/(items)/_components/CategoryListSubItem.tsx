16a34d19038378b88ba69018dd145b0c84419d90
AdamSeungheonShin <ssh9809@naver.com>
2024-12-02 16:44:24 +0900

    FE-Feat: 회의실 삭제 구현

--- apps/web/app/admin/(items)/_components/CategoryListSubItem.tsx
+++ apps/web/app/admin/(items)/_components/CategoryListSubItem.tsx
@@ -2,7 +2,10 @@
 
 import ListItem from "@ui/src/components/common/ListItem";
 import { type IRoom } from "@repo/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { notify } from "@ui/index";
 import { useSidebarStore } from "@/app/store/useSidebarStore";
+import { deleteRoom } from "@/api/meetings";
 import useMeetingsStore from "../_store/useMeetingsStore";
 import CategoryEditDropdown from "./CategoryEditDropdown";
 import ConfirmationModal from "./ConfirmationModal";
@@ -14,6 +17,7 @@ interface CategoryListSubItemProps {
 export default function CategoryListSubItem({ item }: CategoryListSubItemProps): JSX.Element {
   const { isSidebarOpen, openSidebar } = useSidebarStore();
   const { setPanelState, setCurrentItem, setCurrentCategory } = useMeetingsStore();
+  const queryClient = useQueryClient();
 
   const openPanelToEditItem = (selectedItem: IRoom): void => {
     if (!isSidebarOpen) {
@@ -24,10 +28,32 @@ export default function CategoryListSubItem({ item }: CategoryListSubItemProps):
     }
   };
 
+  const mutation = useMutation({
+    mutationFn: async (itemId: string) => {
+      return await deleteRoom(itemId);
+    },
+    onSuccess: async () => {
+      notify({ type: "success", message: "회의실이 삭제되었습니다." });
+      await queryClient.invalidateQueries({ queryKey: ["rooms"] });
+    },
+    onError: () => {
+      notify({ type: "error", message: "회의실 삭제에 실패했습니다. 다시 시도해주세요" });
+    },
+  });
+
+  const handleDeleteRoom = (itemId: string) => {
+    mutation.mutate(itemId);
+  };
   return (
     <ListItem color="white" thickness="thin">
       <span className="flex flex-grow items-center gap-32 text-left">{item.name}</span>
-      <ConfirmationModal title={item.name} type="item" onConfirm={() => {}}>
+      <ConfirmationModal
+        title={item.name}
+        type="item"
+        onConfirm={() => {
+          handleDeleteRoom(item._id);
+        }}
+      >
         <CategoryEditDropdown
           onClickEdit={() => {
             openPanelToEditItem(item);
