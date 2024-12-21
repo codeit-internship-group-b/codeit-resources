df7d8509b622388113d23a2612cb0427c7ade1ea
김보경 <rlaqhrud6263@naver.com>
2024-12-21 23:47:51 +0900

    Chore: CSS 수정

--- apps/web/app/settings/_components/ProfileInfo.tsx
+++ apps/web/app/settings/_components/ProfileInfo.tsx
@@ -9,21 +9,25 @@ export default function ProfileDetails(): JSX.Element {
   const { currentImage, handleImageChange } = useProfileImage(profileImage);
 
   return (
-    <div className="rounded-8 bg-gray-60 flex gap-16 border border-[#E4E3E8] px-16 py-12 md:gap-32 md:border-none md:bg-inherit md:p-0">
-      <div className="text-md-bold md:text-lg-bold md:text-custom-black flex flex-col justify-center gap-4 text-[#818084] md:gap-16">
-        <p>이름</p>
-        <p>이메일</p>
-        <p>팀</p>
+    <>
+      <div className="flex items-center gap-48">
         <p className="text-lg-bold hidden h-72 items-center md:flex">사진</p>
-      </div>
-      <div className="text-lg-regular flex flex-col justify-between gap-4 md:gap-16">
-        <p>{name}</p>
-        <p>{email}</p>
-        <div className="flex items-center gap-8">
-          <ProfileTeams teams={teams} />
-        </div>
         <ProfileImageUploader size="sm" currentImage={currentImage} onImageChange={handleImageChange} />
       </div>
-    </div>
+      <div className="rounded-8 bg-gray-60 flex gap-16 border border-[#E4E3E8] px-16 py-12 md:gap-32 md:border-none md:bg-inherit md:p-0">
+        <div className="text-md-bold md:text-lg-bold md:text-custom-black flex flex-col justify-center gap-4 text-[#818084] md:gap-16">
+          <p>이름</p>
+          <p>이메일</p>
+          <p>팀</p>
+        </div>
+        <div className="text-lg-regular flex flex-col justify-between gap-4 md:gap-16">
+          <p>{name}</p>
+          <p>{email}</p>
+          <div className="flex items-center gap-8">
+            <ProfileTeams teams={teams} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
