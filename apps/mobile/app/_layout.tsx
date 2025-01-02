530b348678606ab0789971982dd230e4fa8bb2db
김보경 <rlaqhrud6263@naver.com>
2025-01-03 00:58:48 +0900

    [Setting] nativewind 세팅 추가 (#179)
    * Fix: nativewind glodal.css 에러 처리
    
    * Setting: nativewind 설정연동 extend packages/tailwind-config
    
    * Fix: ErrorBoundary query remove 로직 수정
    
    * Chore: root tsconfig 삭제

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,9 +4,9 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
-import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
+import "react-native-reanimated";
 import "../global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
--- apps/web/components/common/Fallback/index.tsx
+++ apps/web/components/common/Fallback/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@ui/index";
+import { getQueryClient } from "@/lib/queryClient";
 
 interface ErrorFallbackProps {
   error: Error;
@@ -8,7 +9,10 @@ interface ErrorFallbackProps {
 }
 
 export default function ErrorFallback({ error, resetErrorBoundary }: ErrorFallbackProps): JSX.Element {
+  const queryClient = getQueryClient();
+
   const handleResetErrorBoundary = (): void => {
+    queryClient.removeQueries();
     resetErrorBoundary();
   };
 
