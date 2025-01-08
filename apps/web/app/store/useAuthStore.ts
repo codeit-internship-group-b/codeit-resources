bd1c2bfbf1ad6d70fdbeef2bf421bd694ab5ce18
Lin <heron6534@gmail.com>
2025-01-09 02:29:42 +0900

    Feat: postMessage 처리 추가하여 WebView 로그인/로그아웃 상태 동기화

--- apps/web/app/store/useAuthStore.ts
+++ apps/web/app/store/useAuthStore.ts
@@ -19,10 +19,24 @@ export const useAuthStore = create(
       login: (user: IUser, accessToken: string) => {
         set({ isLoggedIn: true, user });
         setCookie("accessToken", accessToken);
+
+        window.ReactNativeWebView.postMessage(
+          JSON.stringify({
+            type: "LOGIN_SUCCESS",
+            data: { user, accessToken },
+          }),
+        );
       },
       logout: () => {
         set({ isLoggedIn: false, user: null });
         deleteCookie("accessToken");
+
+        window.ReactNativeWebView.postMessage(
+          JSON.stringify({
+            type: "LOGIN_SUCCESS",
+            data: null,
+          }),
+        );
       },
     }),
     {
