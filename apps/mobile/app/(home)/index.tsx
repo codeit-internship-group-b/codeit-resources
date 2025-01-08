7f44886707ee8a85000910cdbac6f46d1e32c6cb
김보경 <rlaqhrud6263@naver.com>
2025-01-08 11:59:45 +0900

    Feat: expo router stack 구성

---
+++ apps/mobile/app/(home)/index.tsx
@@ -0,0 +1,7 @@
+import Constants from "expo-constants";
+import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function HomeScreen() {
+  return <WebView className="flex-1" source={{ uri: "https://codeit.click" }} />;
+}
