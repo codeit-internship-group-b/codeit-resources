3508d40ebbed93732ae1f06413d3929190c3b202
Lin <heron6534@gmail.com>
2025-01-13 15:26:30 +0900

    Refactor: 로그인 API 로직 개선 및 토큰 검증 로직직 추가

--- apps/web/api/auth.ts
+++ apps/web/api/auth.ts
@@ -1,17 +1,27 @@
 import { API_ENDPOINTS } from "@repo/constants";
 import { type FieldValues } from "react-hook-form";
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 import { type SignInResponseType } from "@repo/types/src/responseType";
 
 const isWebView = typeof window !== "undefined" && window.ReactNativeWebView;
 const API_URL = isWebView ? process.env.NEXT_PUBLIC_ANDROID_API_URL : process.env.NEXT_PUBLIC_API_URL;
 
-export const postSignIn = async (payload: FieldValues): Promise<SignInResponseType> => {
-  const { data } = await axios.post<SignInResponseType>(`${API_URL}${API_ENDPOINTS.AUTH.SIGN_IN}`, payload, {
+const callSignInAPI = async (payload: FieldValues | string): Promise<AxiosResponse<SignInResponseType>> => {
+  return axios.post<SignInResponseType>(`${API_URL}${API_ENDPOINTS.AUTH.SIGN_IN}`, payload, {
     headers: {
       "Content-Type": "application/json",
     },
   });
+};
 
+// 로그인 API (데이터 반환)
+export const postSignIn = async (payload: FieldValues): Promise<SignInResponseType> => {
+  const { data } = await callSignInAPI(payload);
   return data;
+};
+
+// 토큰 검증 API (상태 반환)
+export const validateToken = async (payload: string): Promise<boolean> => {
+  const { status } = await callSignInAPI(payload);
+  return status === 200;
 };
--- apps/web/app/utils/webviewLoginBridge.ts
+++ apps/web/app/utils/webviewLoginBridge.ts
@@ -1,27 +1,11 @@
-import { useRouter } from "next/navigation";
-import axios from "axios";
-import { type SignInResponseType } from "@repo/types";
-import { API_ENDPOINTS } from "@repo/constants";
-import type { WebviewMessageType, WebviewLoginMessageHandler } from "@ui/src/types/WebviewMessageTypes";
+import type { WebviewLoginMessageHandler } from "@ui/src/types/WebviewMessageTypes";
+import { WEBVIEW_MESSAGE_TYPES } from "@repo/constants";
+import { validateToken } from "@/api/auth";
 import { useAuthStore } from "../store/useAuthStore";
 
-const webviewLoginBridge = new Map<WebviewMessageType, WebviewLoginMessageHandler>();
+const webviewLoginBridge = new Map<string, WebviewLoginMessageHandler>();
 
-const isWebView = typeof window !== "undefined" && window.ReactNativeWebView;
-const API_URL = isWebView ? process.env.NEXT_PUBLIC_ANDROID_API_URL : process.env.NEXT_PUBLIC_API_URL;
-
-const validateToken = async (payload: string): Promise<boolean> => {
-  const response = await axios.post<SignInResponseType>(`${API_URL}${API_ENDPOINTS.AUTH.SIGN_IN}`, payload, {
-    headers: {
-      "Content-Typet": "application/json",
-    },
-  });
-
-  return response.status === 200;
-};
-
-webviewLoginBridge.set("AUTO_LOGIN", async (data) => {
-  const router = useRouter();
+webviewLoginBridge.set(WEBVIEW_MESSAGE_TYPES.AUTO_LOGIN, async (data) => {
   const { login } = useAuthStore();
   const { user, accessToken } = data;
 
@@ -29,7 +13,6 @@ webviewLoginBridge.set("AUTO_LOGIN", async (data) => {
 
   if (isValidToken) {
     login(user, accessToken);
-    router.push("/dashboard");
   }
 });
 
