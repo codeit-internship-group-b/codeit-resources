b1866a91fc1a6be5d383e454cae52aee16e90c65
Lin <heron6534@gmail.com>
2025-01-16 13:54:24 +0900

    Chore: AuthGuard에서 웹뷰 리다이렉트 로직 제거

--- apps/web/app/_components/AuthGuard.tsx
+++ apps/web/app/_components/AuthGuard.tsx
@@ -2,20 +2,15 @@
 
 import { useEffect, useState } from "react";
 import { PAGE_NAME } from "@ui/src/utils/constants/pageNames";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/app/store/useAuthStore";
 import { getMessagesFromNative } from "../utils/reactNativeMessage";
-import { useWebView } from "../_hooks/useWebView";
 import SignInForm from "./SignInForm";
 
 export default function AuthGuard(): JSX.Element | null {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
   const { isLoggedIn } = useAuthStore();
-
-  // 웹뷰 작업 추가
-  const { isWebView } = useWebView();
-  if (isWebView) redirect(PAGE_NAME.DASHBOARD);
 
   useEffect(() => {
     getMessagesFromNative();
