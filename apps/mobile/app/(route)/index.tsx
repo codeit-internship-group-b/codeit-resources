5d45e13f43392f8c9876b85479bc489c353fa728
Lin <heron6534@gmail.com>
2025-01-16 13:57:38 +0900

    Feat: WebView 메시지에 따른 분기처리

--- apps/mobile/app/(route)/index.tsx
+++ apps/mobile/app/(route)/index.tsx
@@ -1,17 +1,39 @@
+import { WEBVIEW_MESSAGE_TYPES } from "@repo/constants";
+import { useRef } from "react";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 
 import { useHandleNavigationActions } from "@/hooks/useHandleNavigationActions";
-import { getBaseUrl } from "@/utils/getBaseUrl";
+import { handleReceiveMessage, handleLoadProgressCurried, handleLoadCurried } from "@/utils/bridgeHandler";
+import { getNativeApiUrl } from "@/utils/getNativeApiUrl";
+import { parseMessage } from "@/utils/parseMessage";
 
 export default function HomeScreen() {
-  // TODO : login상태에 따른 분기 처리 설정
+  const webviewRef = useRef<WebView>(null);
 
-  const baseUrl = getBaseUrl();
+  const baseUrl = getNativeApiUrl();
   const handleNavigationActions = useHandleNavigationActions();
 
-  const requestOnMessage = (e: WebViewMessageEvent) => {
-    handleNavigationActions(e);
+  const handleWebviewMessage = (e: WebViewMessageEvent) => {
+    const { type } = parseMessage(e);
+
+    if (type === WEBVIEW_MESSAGE_TYPES.ROUTER_EVENT) {
+      handleNavigationActions(e);
+    } else {
+      handleReceiveMessage(e);
+    }
   };
 
-  return <WebView className="flex-1" source={{ uri: `${baseUrl}` }} onMessage={requestOnMessage} />;
+  return (
+    <WebView
+      ref={webviewRef}
+      className="flex-1"
+      source={{ uri: `${baseUrl}` }}
+      onMessage={handleWebviewMessage}
+      onLoadProgress={handleLoadProgressCurried(webviewRef)}
+      onLoad={handleLoadCurried(webviewRef)}
+      cacheEnabled
+      javaScriptEnabled
+      domStorageEnabled
+    />
+  );
 }
